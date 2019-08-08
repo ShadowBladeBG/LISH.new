7820c0249785db3d005782dac85f796c713dadc6
ShadowBladeBG <53444234+ShadowBladeBG@users.noreply.github.com>
2019-08-08 21:27:49 +0300

    Add files via upload

---
+++ fourth.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<cstdlib>
+#include<math.h>
+#include<random>
+#pragma once
+#include<cmath>
+#include<utility>
+#include<algorithm>
+#include<string>
+#include<fstream>
+using namespace std;
+int countSpaces(char space);
+//int third(int argc, char *argv[]);
+int fourth()
+{
+    char space;
+    
+    cout<<" Spaces\t\tHow many times"<<endl;
+    
+    for (space = ' '; space<= ' '; space++)
+    {
+        cout<<space<<"\t\t"<<countSpaces(space)<<"times"<<endl;
+    }
+    system("PAUSE");
+    return EXIT_SUCCESS;
+    
+
+}
+int countSpaces(char space)
+{
+    ifstream stream;
+    char spacing;
+    int counterforspacing = 0;
+    
+    stream.open("Plain.txt");
+    //stream.open("chipher2.txt");
+    stream.get(spacing);
+    
+    while(!stream.fail())
+    {
+        if(spacing == space)
+            counterforspacing++;
+        stream.get(spacing);
+    }
+    stream.close();
+    return counterforspacing;
+}
