4c09cb4538264bd11c6480ec4f3c87e2e4583ea4
ShadowBladeBG <53444234+ShadowBladeBG@users.noreply.github.com>
2019-08-13 03:01:25 +0300

    Add files via upload

---
+++ isprintable.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<cstdlib>
+#include<math.h>
+#include<random>
+#pragma once
+#include<cmath>
+#include<utility>
+#include<algorithm>
+#include<string>
+#include<fstream>
+#include <stdio.h>
+#include <ctype.h>
+int countPrint(char printable);
+//int third(int argc, char *argv[]);
+int main()
+{
+    char printable;
+    std::cout<<" Numbers\t\tHow many times"<<std::endl;
+    std::ifstream isprint;
+    int counterforprintable = 0;
+    
+    isprint.open("Plain.txt");
+    //stream.open("chipher2.txt");
+    isprint.get(printable);
+    
+    while(!isprint.fail())
+    {
+        if(printable == printable)
+            counterforprintable++;
+        isprint.get(printable);
+    }
+    isprint.close();
+    std::cout<<counterforprintable<<std::endl;
+    return counterforprintable;
+}
+
+/*
+{
+  int i=0;
+  char str[]="first line \n second line \n";
+  while (isprint(str[i]))
+  {
+    putchar (str[i]);
+    i++;
+  }
+  return 0;
+}
+*/
